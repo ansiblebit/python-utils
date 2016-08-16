b75354d621e1e3f2de3363f7328e893227dcef31
Pedro Salgado <steenzout@ymail.com>
2016-08-16 11:21:44 -0600

    updated code on namespace package.

--- company/__init__.py
+++ company/__init__.py
@@ -2,4 +2,9 @@
 """
 Company namespace package.
 """
-__import__('pkg_resources').declare_namespace(__name__)
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
