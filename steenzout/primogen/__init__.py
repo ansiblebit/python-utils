e7af10352b4dc9a9b60aac6eb18a2db795f3c937
Pedro Salgado <steenzout@ymail.com>
2016-10-18 18:56:00 -0600

    updated docstring.

--- steenzout/primogen/__init__.py
+++ steenzout/primogen/__init__.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-.. module:: steenzout.primogen
-    :platform: Unix
-    :synopsis: Company primogen.
+"""steenzout.primogen package."""
 
-.. moduleauthor:: Your Name <email address>
-"""
 
 from steenzout.primogen.metadata import __version__
 
 
 def version():
-    """
-    Return this primogen version.
+    """Return the package version.
 
-    :return: primogen version.
-    :rtype: str
+    Returns:
+        (str): package version.
     """
     return __version__
