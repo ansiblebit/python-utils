bb009050f0db01fa2c81525eb89177fef4c9375f
Pedro Salgado <steenzout@users.noreply.github.com>
2018-02-13 20:58:29 -0700

    added test stub.

--- tests/unit/oracle_java/__init__.py
+++ tests/unit/oracle_java/__init__.py
@@ -1,2 +1,24 @@
 # -*- coding: utf-8 -*-
 """Unit tests for ansiblebit.lib.oracle_java package."""
+
+from ansiblebit.lib import oracle_java
+
+
+def test_parse_url_jdk8():
+    expected = []
+    out = oracle_java.parse_url(oracle_java.URL_JDK_8)
+
+
+def test_parse_url_jdk9():
+    expected = []
+    out = oracle_java.parse_url(oracle_java.URL_JDK_9)
+
+
+def test_parse_url_jre8():
+    expected = []
+    out = oracle_java.parse_url(oracle_java.URL_JRE_8)
+
+
+def test_parse_url_jre9():
+    expected = []
+    out = oracle_java.parse_url(oracle_java.URL_JRE_9)
